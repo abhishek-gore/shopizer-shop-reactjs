0e7f032e995cd6d09dd8e5690702cbb509bb3aa5
abhishek-gore <abhishek.gore@technogise.com>
2026-03-31 11:33:48 +0530

    Fix test cases - add proper Redux store mocks

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import { createMultilanguageReducer } from 'redux-multilanguage';
 import App from './App';
 
-const mockStore = createStore(() => ({}));
+const mockReducer = combineReducers({
+  multilanguage: createMultilanguageReducer({ currentLanguageCode: 'en' }),
+  productData: () => ({ products: [] }),
+  merchantData: () => ({}),
+  cartData: () => ({ cartItems: [] }),
+  loading: () => ({ isLoading: false }),
+  userData: () => ({}),
+  content: () => ({})
+});
+
+const mockStore = createStore(mockReducer);
 
 test('renders without crashing', () => {
   render(
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+window._env_ = {
+  APP_BASE_URL: 'http://localhost:8080',
+  APP_API_VERSION: '/api/v1',
+  APP_MERCHANT: 'DEFAULT',
+  APP_THEME_COLOR: '#D1D1D1'
+};
+
+window.scrollTo = jest.fn();
